3dd8acfaf4214c12798ba00586521d5979bc1179
sbarbett <shane.barbetta@neustar.biz>
2015-05-01 17:33:07 -0400

    fix

--- bulk_ttl_update.py
+++ bulk_ttl_update.py
@@ -1,7 +1,7 @@
 import csv, ultra_rest_client, json, sys, time
 
 if len(sys.argv) == 2 and sys.argv[1].lower() == "help":
-    sys.exit("Expected use: python bulk_ttl_update.py username password example.csv [use_http host:port]\nArgument 1:\n\tbulk_ttl_update.py -- The name of your python file\nArgument 2:\n\tusername -- Username of the UltraDNS account\nArgument 3:\n\tpassword -- UltraDNS account password\nArgument 4:\n\texample.csv -- The CSV file containing your update information (see example.csv).\nArguments 5 and 6 (optional):\n\tuse_http -- Specify this value as 'True' if you wish to use a test environment\n\thost:port -- The hostname and port of your test environment (Example: test-restapi.ultradns.com:443)\n")
+    sys.exit("Expected use: python bulk_ttl_update.py username password example.csv [use_http host:port]\nArgument 1:\n\tbulk_ttl_update.py -- The name of your python file\nArgument 2:\n\tusername -- Username of the UltraDNS account\nArgument 3:\n\tpassword -- UltraDNS account password\nArgument 4:\n\texample.csv -- The CSV file containing your update information (see example.csv)\nArguments 5 and 6 (optional):\n\tuse_http -- Specify this value as 'True' if you wish to use a test environment\n\thost:port -- The hostname and port of your test environment (Example: test-restapi.ultradns.com:443)\n")
     
 if len(sys.argv) != 6 and len(sys.argv) != 4:
     raise Exception("Expected use: python bulk_ttl_update.py username password example.csv [use_http host:port]\n\nType 'python bulk_ttl_update.py help' for more information.\n")
